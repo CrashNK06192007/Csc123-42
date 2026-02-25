b008a570fdbd750d33bced18cd07ebbb6dc595b6
CrashNK06192007 <julianator5000@icloud.com>
2026-02-25 02:02:50 +0000

    jgonzalez7.py

---
+++ jgonzalez7.py
@@ -0,0 +1,19 @@
+# Julian Gonzalez
+# Lab #7
+
+x = 5
+y = 6.45 
+str1 = "Hello"
+
+if x > 6:
+    print("X is Greater than 6")
+elif x < 6:
+    print("X is Lower than 6")
+
+if y > 10:
+    print("Y is Greater than 10")
+elif x < 7:
+    print("Y is lower than 6, the value for y was", y,)
+
+if str1 == "Hello":
+    print("str1 is equal to",str1, "chat")
